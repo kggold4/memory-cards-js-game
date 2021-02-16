af84df8c438f4efd771318a1322fc94ee890d872
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-02-16 16:44:20 +0200

    small updates

--- js/mainJS.js
+++ js/mainJS.js
@@ -1,13 +1,20 @@
-//getting html elements
-function getDOMs() {
-    const main = document.getElementById("main");
-    const range = document.getElementById("range");
-    const limitOutput = document.getElementById("limitOutput");
-    const scoreOutput = document.getElementById("scoreOutput");
-    const clicks = document.getElementById("clicks");
-    const win = document.getElementById("win");
+//stating game
+function start() {
+    getDOMs();
+    changeLimit();
 }
 
+//getting html elements
+function getDOMs() {
+    const win = document.getElementById("win");
+    const main = document.getElementById("main");
+    const range = document.getElementById("range");
+    const clicks = document.getElementById("clicks");
+    const limitOutput = document.getElementById("limitOutput");
+    const scoreOutput = document.getElementById("scoreOutput");
+}
+
+// cahnge limit range of the cards
 let limit;
 function changeLimit() {
     if(range.value % 2 ==0) {
@@ -19,12 +26,7 @@ function changeLimit() {
     }
 }
 
-//stating game
-function start() {
-    changeLimit();
-}
-
-//play game
+// play game
 function play() {
     score = -2;
     scoreCount();
